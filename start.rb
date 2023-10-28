e408da03c0c9e51d6b52bdff4f2b43ff23a7f8b6
Luciano Scarpaci <tauxsik@gmail.com>
2023-10-27 23:39:59 -0400

    Update model and prompt

--- start.rb
+++ start.rb
@@ -10,26 +10,26 @@ class ChatGPT
     OpenAI.configure do |c|
       c.access_token = ENV.fetch('OPENAI_KEY')
       @client = OpenAI::Client.new
-      #puts client.models.retrieve(id: 'text-davinci-003')
-      #puts client.models.retrieve(id: 'gpt-3.5-turbo-0301')
+      # puts client.models.retrieve(id: 'text-davinci-003')
+      # puts client.models.retrieve(id: 'gpt-3.5-turbo-0301')
     end
   end
 
   def chat_response
     retry_count = 0
     max_retries = 9999
-    #client = OpenAI::Client.new
-    #@client
+    # client = OpenAI::Client.new
+    # @client
     while retry_count <= max_retries
       begin
         response = @client.completions(
           parameters: {
-            model: 'text-davinci-003',
-            prompt: 'what is 4 + 7?',
-            max_tokens: 4000,
+            model: 'gpt-3.5-turbo-instruct',
+            prompt: 'Wubby: You are a friendly companion who cares about my well being. Friend: You are an amazing companion!',
+            max_tokens: 4000
           }
         )
-        puts response["choices"].map { |c| c["text"] }
+        puts(response['choices'].map { |c| c['text'] })
         break
       rescue OpenAI::Error => e
         retry_count += 1
